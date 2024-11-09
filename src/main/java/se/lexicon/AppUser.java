5c6264821c2befbc55738298e39b399575ae584e
Fatemeh <f.khalleghi.777@gmail.com>
2024-11-09 02:59:15 +0100

    AppUser2

--- src/main/java/se/lexicon/AppUser.java
+++ src/main/java/se/lexicon/AppUser.java
@@ -1,18 +1,20 @@
 package se.lexicon;
 
+import java.util.UUID;
+
 public class AppUser {
     private String username;
     private String password;
     private AppRole role;
 
     public AppUser(String username, String password, AppRole role) {
+        this.generatePassword();
         this.username = username;
         this.password = password;
         this.role = role;
     }
     public AppUser(String username, String password) {
-        this.username = username;
-        this.password = password;
+        this(username, password, null);
     }
 
     public String getUsername() {
@@ -26,10 +28,14 @@ public class AppUser {
     }
 
     public void setUsername(String username) {
+        if(username == null || username.trim().isEmpty())
+            throw new IllegalArgumentException("Username cannot be null or empty");
         this.username = username;
     }
 
     public void setPassword(String password) {
+        if(password == null || password.trim().isEmpty())
+            throw new IllegalArgumentException("Password cannot be null or empty");
         this.password = password;
     }
     public void setRole(AppRole role) {
@@ -44,6 +50,13 @@ public class AppUser {
     }
     public String toString() {
         return this.username;
+    }
+
+    public void generatePassword() {
+        this.password = UUID.randomUUID().toString();
+    }
+    public String userInfo(){
+        return "Name is: " + this.username +" Password is: " + this.password + " ID is: " + this.role;
     }
 
 
--- src/main/java/se/lexicon/Main.java
+++ src/main/java/se/lexicon/Main.java
@@ -5,6 +5,21 @@ package se.lexicon;
 public class Main {
     public static void main(String[] args) {
 
+        AppUser user1 = new AppUser("John", "Doe");
+
+        System.out.println("hashCode is: " + user1.hashCode());
+        System.out.println(user1.toString());
+        user1.generatePassword();
+        System.out.println(user1.userInfo());
+        System.out.println("------------");
+        AppUser user2 = new AppUser("Peter", "Will");
+        System.out.println("hashCode is: " + user2.hashCode());
+        System.out.println("generatePassword is: " + user2.toString());
+
+
+
+
+
 
 
         }
